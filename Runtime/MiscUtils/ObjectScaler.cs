022151803b90450a4965777a380aebc8cb37b422
ajnaduvil <ajnaduvil@gmail.com>
2020-02-23 15:40:21 +0530

    Object Scaler Added

---
+++ Runtime/MiscUtils/ObjectScaler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AVR.Utils
+{
+    /// <summary>
+    /// Scales the target object
+    /// </summary>
+    public class ObjectScaler : MonoBehaviour
+    {
+        public GameObject target;
+        private Vector3 scale;
+
+        #region Properties
+        /// <summary>
+        /// Use the property to instanly apply the scale
+        /// </summary>
+        public Vector3 Scale { get => scale; set => ApplyScale(value); }
+        #endregion
+
+        private void Start()
+        {
+            ApplyScale(Scale);
+        }
+
+        private void ApplyScale(Vector3 newScale)
+        {
+            target.transform.localScale = newScale;
+            scale = newScale;
+        }
+    }
+}
