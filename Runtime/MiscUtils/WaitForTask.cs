a1d7bccc28f479ec99d2bae133bc84d213c9c3f2
Albin Jose <ajnaduvil@gmail.com>
2022-05-01 19:35:27 +0530

    waitfor task added

---
+++ Runtime/MiscUtils/WaitForTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WaitForTask : CustomYieldInstruction
+{
+    private readonly Task task;
+
+    public WaitForTask(Task task)
+    {
+        this.task = task;
+    }
+    public override bool keepWaiting
+    {
+        get
+        {
+            if (!task.IsCompleted)
+            {
+                return true;
+            }
+            if (task.IsFaulted)
+            {
+                AggregateException exception = task.Exception;
+                Debug.LogException(exception);
+            }
+            return false;
+        }
+    }
+}
